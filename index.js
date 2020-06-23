82aebd758094ee88f9cc9e571d2c5cb95f89102e
Lens <lensmutombo@gmail.com>
2020-06-23 14:42:54 +0200

    added resolvers and typedefs

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const {ApolloServer} = require('apollo-server');
+const gql = require('graphql_tag');
+
+const typeDefs = gql`
+    type Query{
+        direBonjour: String! 
+    }
+`
+const resolvers = {
+    Query:{
+        sayHi:() => 'Bonjour Tout le monde'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen({ port:5000})
+    .then(res => {
+        console.log(`Server running at ${res.url}`);
+    });
